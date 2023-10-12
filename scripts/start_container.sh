cc14ba547e7431944f31a98592933b132553f92b
Saradappk <64032672+Saradappk@users.noreply.github.com>
2023-10-12 10:34:18 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,5 +3,5 @@ set -e
 
 # Pull the Docker image from Docker Hub
 docker pull sarada29/simple-python-flask-app
- # Run the Docker image as a container
+# Run the Docker image as a container
 docker run -d -p 5000:5000 sarada29/simple-python-flask-app
